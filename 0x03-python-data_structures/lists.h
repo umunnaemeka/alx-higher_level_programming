31624b1433e2cf4f8dfdb0d5f95cb7eb04b8640c
umunnaemeka <umunnaemeka@gmail.com>
2022-12-07 02:11:45 -0800

    review

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LISTS_H_
+#define LISTS_H_
 
 #include <stdlib.h>
 
@@ -20,9 +20,11 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+
+
 int is_palindrome(listint_t **head);
-listint_t *reverse_linked_list(listint_t **head);
-void rev_list(listint_t **head);
-void print_python_list_info(PyObject *p);
+
+
+
 
 #endif /* LISTS_H */
