d8822d0cf52869e9b0fdb6dcf9a3853f16f0c692
umunnaemeka <umunnaemeka@gmail.com>
2023-03-15 20:32:32 -0700

    Java

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argv = require('process').argv;
+// prints a message depending of the number of arguments passed.
 
-if (argv.length === 3) {
-  console.log('Argument found');
-} else if (argv.length > 3) {
-  console.log('Arguments found');
-} else {
+if (process.argv.length < 3) {
   console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
